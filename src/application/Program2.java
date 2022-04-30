0ed5e532f9f929f1c26a86dbc988a613c148a072
Tiago Varga <tiagofvdesign@gmail.com>
2022-04-30 14:31:34 -0300

    DeleteById from department implemented

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -14,21 +14,32 @@ public static void main(String[] args) {
 
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-		System.out.println("=== Test 1: seller findById =====" );
+		System.out.println("=== Test 1: department findById =====" );
 		Department department = departmentDao.findById(1);		
 		System.out.println(department);
 		
-		System.out.println("\n=== Test 2: seller findAll =====" );
+		System.out.println("\n=== Test 2: department findAll =====" );
 		List<Department> list = departmentDao.findAll();
 		for (Department dep : list) {
 			System.out.println(dep);
 		}
 		
-		System.out.println("\n=== Test 3: seller insert =====" );
+		System.out.println("\n=== Test 3: department insert =====" );
 		Department newDepartment = new Department(null , "Sports");
 		departmentDao.insert(newDepartment);
 		System.out.println("Tnserted! New id = " + newDepartment.getId());
 		
+		System.out.println("\n=== Test 5: department update =====" );
+		newDepartment = departmentDao.findById(1);
+		newDepartment.setName("Foods");
+		departmentDao.update(newDepartment);
+		System.out.println("Update completed");
+		
+		System.out.println("\n=== Test 6: department delete =====" );
+		System.out.println("Enter id for delected test");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
 		
 		sc.close();
 
